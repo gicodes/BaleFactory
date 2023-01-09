ec0cf441f95adf1d5665c1a2283c96e458e33c05
Gideon, I <104095690+gicodes@users.noreply.github.com>
2023-01-09 12:44:09 +0100

    Update repository.js

--- repositories/repository.js
+++ repositories/repository.js
@@ -1,5 +1,14 @@
-const fs = require('fs');
 const crypto = require('crypto');
+const fs = import { readFileSync } from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), 'files', 'test.json');
+  const stringified = readFileSync(file, 'utf8');
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(stringified);
+}
 
 module.exports = class Repository {
     constructor(filename) {
