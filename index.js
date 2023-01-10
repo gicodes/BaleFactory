4c89d06469cb716d36fe29c4bb564eac7195d6cd
Gideon, I <104095690+gicodes@users.noreply.github.com>
2023-01-10 19:14:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const AdminproductRouter = require('./routes/admin/products');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
