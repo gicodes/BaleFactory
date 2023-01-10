da05f977739f3d0c6c3b4f01d023cc4c1347fccb
Gideon, I <104095690+gicodes@users.noreply.github.com>
2023-01-10 20:28:07 +0100

    Update prodIndex.js

--- views/products/prodIndex.js
+++ views/products/prodIndex.js
@@ -7,15 +7,20 @@ module.exports = ({ content }) => {
           <header class="container">
             <nav class="navbar navbar-bottom">
               <div class="container navbar-container">
-                <div>
-                  <a href="/">
-                    <p class="subtitle">Click here to read more on Bale Factory</p>
-                  </a>
-                </div>
                 <div class="navbar-item">
                   <div class="navbar-buttons">
                     <div class="navbar-item">
                       <a href="/products"><i class="fa fa-star"></i> Products</a>
+                    </div>
+                    <div class="navbar-item">
+                      <a href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="navbar level">
+                  <div class="level-right">
+                    <div class="navbar-item">
+                      <a href="/products"><i class="fa fa-user"></i> Login</a>
                     </div>
                     <div class="navbar-item">
                       <a href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
@@ -31,6 +36,11 @@ module.exports = ({ content }) => {
               <div class="columns is-centered">
                 <img src="/images/banner.jpg" />
               </div>
+            </div>
+            <div>
+              <a href="/">
+                <p class="subtitle">Click here to read more on Bale Factory</p>
+              </a>
             </div>
           </section>
 
