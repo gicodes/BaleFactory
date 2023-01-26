60bba319182c4c9e3aab0648f9f273d4a5e676ff
Gideon, I <104095690+gicodes@users.noreply.github.com>
2023-01-26 23:40:26 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,4 +1,13 @@
-export default function handler(request, response) {
+import { readFileSync } from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const file = path.join(process.cwd(), 'files', 'test.json');
+  const stringified = readFileSync(file, 'utf8');
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.end(stringified)
+  
   response.status(200).json({
     body: request.body,
     query: request.query,
