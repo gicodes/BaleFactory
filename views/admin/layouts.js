32c538f44c5fe38418bb385e83528c6a22269424
Gideon, I <gicodes9@gmail.com>
2023-11-09 18:08:43 +0000

    making absolutely obsolete

--- views/admin/layouts.js
+++ views/admin/layouts.js
@@ -12,29 +12,104 @@ module.exports = ({ content }) => {
 			</head>
 		
 			<body>
-				<header>
-					<nav class="navbar navbar-top vert-space">
-						<div class="container navbar-container">
-							<div class="navbar-end">
-								<div class="navbar-item">
-									<ul class="social is-size-5">
-										<li><a href="https://github.com/gicodes"><i class="fab fa-github"></i></a></li>
-										<li><a href="/"><i class="fab fa-facebook"></i></a></li>
-										<li><a href="https://linkedin.com/in/gideon-iduma-5311445a"><i class="fab fa-linkedin"></i></a></li>
-										<li><a href="/"><i class="fab fa-dribbble"></i></a></li>
-										<li><a href="https://twitter.com/pappichino"><i class="fab fa-twitter"></i></a></li>
-									</ul>
-								</div>
+				<nav class="navbar container" role="navigation" aria-label="main navigation">
+					<div class="navbar-menu is-active">
+			
+						<div class="navbar-start">
+							<div class="tags has-addons">
+							<span class="tag is-medium">
+								<a href="/products"><i class="fa fa-star"></i> Products</a>
+							</span>
+							<span class="tag is-link is-medium">
+								<a class='has-text-white' href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
+							</span>
 							</div>
 						</div>
-					</nav>
-				</header>
-
-
-				<div class="container">
-					${content}
+			
+						<div class="level-item">
+							<div class="field has-addons">
+							<p class="control">
+								<input class="input" type="text" placeholder="Find an item">
+							</p>
+							<p class="control">
+								<button class="button">
+								Search
+								</button>
+							</p>
+							</div>
+						</div>
+			
+						<div class="navbar-end">
+							<div class="tags has-addons">
+							<span class="tag is-link is-medium">
+								<a class='has-text-white' href="/signin"> <b>Login</b> </a>
+							</span>
+							<span class="tag is-light is-medium">
+								<a href="/signup"> Sign up </a>
+							</span>
+						</div>
+					</div>
 				</div>
+			</nav>
+			
+			<hr/>
+			
+			<div>
+				${content}
+			</div>
+
+			<section class="hero is-fullheight">
+
+
 				
+					<div class="hero-body">
+						
+						<div class="container has-text-centered">
+							This website is obsolete and is being used as a placehoder to sample site, past project or portfolio 
+						</div>
+					</div>
+
+						<nav class="level container social is-size-4">
+							<div class="level-item has-text-centered">
+								<div>
+									<a href="https://github.com/gicodes"><i class="fab fa-github"></i></a>
+								</div>
+							</div>
+									<hr/>
+								<br/>	
+							<div class="level-item has-text-centered">
+								<div>
+									<a href="/"><i class="fab fa-facebook"></i></a>
+								</div>
+							</div>
+										
+							<div class="level-item has-text-centered">
+								<div>
+									<a href="https://linkedin.com/in/gideon-iduma-5311445a"><i class="fab fa-linkedin"></i></a>
+								</div>
+							</div>
+										
+							<div class="level-item has-text-centered">
+								<div>
+									<a href="/"><i class="fab fa-dribbble"></i></a>
+								</div>
+							</div>
+										
+							<div class="level-item has-text-centered">
+								<div>
+									<a href="https://twitter.com/pappichino"><i class="fab fa-twitter"></i></a>
+								</div>
+							</div>
+						</nav>
+
+					<div class="hero-footer">
+						<div class="notification is-link">
+							<div class="is-size-7 has-text-centered">
+								For more information or inquiry, contact the owner of this website. If you are the owner of this website, ensure to regularly update your projects.
+							</div>
+						</div>
+					</div>
+				</section>
 			</body>
 	  </html>
 	`;
--- views/products/prod+index.js
+++ views/products/prod+index.js
@@ -30,46 +30,6 @@ module.exports = ({ products }) => {
   return layout({
     content: `
       <body>
-        <nav class="navbar container vert-space" role="navigation" aria-label="main navigation">
-          <div class="navbar-menu">
-
-            <div class="navbar-start">
-              <div class="tags has-addons">
-                <span class="tag is-medium">
-                  <a href="/products"><i class="fa fa-star"></i> Products</a>
-                </span>
-                <span class="tag is-link is-medium">
-                  <a class='has-text-white' href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
-                </span>
-              </div>
-            </div>
-
-            <div class="level-item">
-              <div class="field has-addons">
-                <p class="control">
-                  <input class="input" type="text" placeholder="Find an item">
-                </p>
-                <p class="control">
-                  <button class="button">
-                    Search
-                  </button>
-                </p>
-              </div>
-            </div>
-
-            <div class="navbar-end">
-              <div class="tags has-addons">
-                <span class="tag is-link is-medium">
-                  <a class='has-text-white' href="/signin"> <b>Login</b> </a>
-                </span>
-                <span class="tag is-light is-medium">
-                  <a href="/signup"> Sign up </a>
-                </span>
-              </div>
-            </div>
-          </div>
-        </nav>
-
         <div class='card'>
           <div class='container'>
             <section class="card-content">
